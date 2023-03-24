bf5fe757140d179fe7c589a89664ac5d297dbc24
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-03-24 17:41:47 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cambiando informacion
+          
         </a>
       </header>
     </div>
