da64e95e333b8390aae18a200c42900b807982ce
juanfacundotam <facutam@gmail.com>
2023-03-24 17:48:59 -0300

    nuevo cambio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cambiando informacion
+          Nuevo Proyecto
           
         </a>
       </header>
