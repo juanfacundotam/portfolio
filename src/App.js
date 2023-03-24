67992bb717a177b1b1b97cb332b040feb75fd4cb
juanfacundotam <facutam@gmail.com>
2023-03-24 18:29:56 -0300

    Actualizacion Componentes Views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from  "./views/Home/Home"
+import Proyects from "./views/Proyects/Proyects"
+import Contact from "./views/Contact/Contact"
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nuevo Proyecto
-          
-        </a>
+        <Nav/>
+        <Home/>
+        <Proyects/>
+        <Contact/>
+        <Footer/>
       </header>
     </div>
   );
