cb7fef0888d2cbb282c5c1e9ce8a4109c3f1fcc0
juanfacundotam <facutam@gmail.com>
2023-04-02 19:08:46 -0300

    avances cards

--- src/components/ModalProyects/ModalProyects.jsx
+++ src/components/ModalProyects/ModalProyects.jsx
@@ -7,30 +7,39 @@ const ModalProyects = (props) => {
   return (
     <div>
       <Modal
-        className={style.divModal}
+        className={style.modalContainer}
         {...props}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-          <Modal.Header closeButton className={style.headerDiv}>
+          {/* <Modal.Header closeButton className="divModalHeader">
             <Modal.Title id="contained-modal-title-vcenter">
               <h2>{props.tittle}</h2>
             </Modal.Title>
           </Modal.Header>
-          <Modal.Body className={style.bodyDiv}>
+          <Modal.Body className="divModalBody">
             <h4>Description: </h4>
             <p>{props.description}</p>
           </Modal.Body>
-          <Modal.Footer className={style.footerDiv}>
+          <Modal.Footer  >
+            <div className="divModalFooter">
             <h2>Tecnologies: </h2>
             <ul className={style.lista}>
               {props.tecnology.map((icon) => {
                 return <li className={style.iconos} >{icon}</li>;
               })}
             </ul>
+            </div>
             <Button onClick={props.onHide} className={style.button}><span>Sitio Web</span></Button>
-          </Modal.Footer>
+          </Modal.Footer> */}
+          <div className={style.divModal}>
+            <div className={style.modal}>
+            <h1>modal</h1>
+            <h1>modal</h1>
+            <h1>modal</h1>
+            </div>
+          </div>
       </Modal>
     </div>
   );
