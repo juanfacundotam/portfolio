23ed27e23a13f6fb06125e1c8e3d7083bb0f11b4
juanfacundotam <facutam@gmail.com>
2023-04-03 16:49:09 -0300

    avances cards

--- src/components/CardProyect/CardProyect.jsx
+++ src/components/CardProyect/CardProyect.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import style from "./CardProyect.module.css";
 import Modal from "../Modal/Modal";
-
+import {motion} from "framer-motion"
 const proyectsImage = require.context("../../assets/proyectsImage");
 
 const CardProyect = ({
@@ -16,21 +16,32 @@ const CardProyect = ({
 
   const openModal = () => {
     setIsOpen(true);
-
   };
   const closeModal = () => {
     setIsOpen(false);
-
   };
 
   return (
-    <div className={style.containerCard}>
-      <button onClick={openModal}>x</button>
+    <div>
+      <motion.div
+        className={style.divCard}
+        onClick={openModal}
+        initial={{ opacity: 0.6, scale: 0.9 }}
+        transition={{ duration: 0.6 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+      >
+        <h1>{tittle}</h1>
+        <img src={proyectsImage(`./${image}.jpg`)} alt={imagealt} />
+      </motion.div>
       <Modal
-      tittle={tittle}
-      isOpen={isOpen}
-      closeModal={closeModal}
-      setIsOpen={setIsOpen}
+        id={id}
+        tittle={tittle}
+        image={image}
+        description={description}
+        tecnology={tecnology}
+        isOpen={isOpen}
+        closeModal={closeModal}
+        setIsOpen={setIsOpen}
       />
     </div>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,17 @@
 import style from "./Modal.module.css";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+const proyectsImage = require.context("../../assets/proyectsImage");
 
-const Modal = ({ isOpen, closeModal, tittle }) => {
-  
-    const handleModalContainerClick = (event) => {
+const Modal = ({
+  isOpen,
+  closeModal,
+  tittle,
+  image,
+  description,
+  imageAlt,
+  tecnology,
+}) => {
+  const handleModalContainerClick = (event) => {
     event.stopPropagation();
   };
 
@@ -12,14 +20,27 @@ const Modal = ({ isOpen, closeModal, tittle }) => {
       onClick={closeModal}
       className={`${style.modal} ${isOpen && style.isOpen}`}
     >
-      <motion.div initial={{ opacity: 0.8, y: "-60px" }}
-        transition={{ duration: 0.6}}
+      <motion.div
+        initial={{ opacity: 0.8, y: "-60px" }}
+        transition={{ duration: 0.6 }}
         whileInView={{ y: "0px", opacity: 1 }}
-      className={style.modalContainer} onClick={handleModalContainerClick}>
+        className={style.modalContainer}
+        onClick={handleModalContainerClick}
+      >
         <button onClick={closeModal} className={style.modalClose}>
           X
         </button>
         <h1>{tittle}</h1>
+        <img src={proyectsImage(`./${image}.jpg`)} alt={imageAlt} />
+        <p>{description}</p>
+        <div>
+          <ul className={style.list}>
+            {tecnology.map((tecno) => {
+              return <li className={style.item}>{tecno}</li>;
+            })}
+          </ul>
+        </div>
+        <button className={style.btnWeb}>Web Site</button>
       </motion.div>
     </article>
   );
